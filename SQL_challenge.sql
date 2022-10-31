4c99d5f0aec742ea85792c0db57ae4596158a882
Gabriele Pedicelli <gabepedicelli@yahoo.com>
2022-10-31 14:48:37 -0500

    SQL-Challenge

--- SQL_challenge.sql
+++ SQL_challenge.sql
@@ -22,6 +22,13 @@ CREATE TABLE departments (
 	dept_name VARCHAR
 );
 
+DROP TABLE departments 
+
+CREATE TABLE departments (
+	dept_no VARCHAR,
+	dept_name VARCHAR
+);
+
 CREATE TABLE dept_manager(
 	dept_no VARCHAR,
 	emp_no INT
@@ -39,15 +46,3 @@ CREATE TABLE salaries(
 	salary INT
 );
 
---1 List the employee number, last name, first name, sex, and salary of each employee
-SELECT employees.emp_no, employees.last_name, employees.first_name, employees.sex, salaries.salary
-FROM employees 
-JOIN salaries 
-ON (employees.emp_no = salaries.emp_no);
-
---2 List the first name, last name, and hire date for employees who were hired in 1986
-SELECT first_name, last_name, hire_date 
-FROM employees 
-WHERE hire_date BETWEEN '01/01/1986' AND '12/31/1986';
-
---3 List the manager of each department along with their department number, department name, employee number, last name, and first name
