fe243a7f684d3d283956751aa5873baaee6042ec
Gabriele Pedicelli <gabepedicelli@yahoo.com>
2022-11-01 21:12:02 -0500

    SQL-Challenge

--- SQL_Challenge Schema.sql
+++ SQL_Challenge Schema.sql
@@ -2,7 +2,7 @@
 SELECT employees.emp_no, employees.last_name, employees.first_name, employees.sex, salaries.salary
 FROM employees 
 JOIN salaries 
-ON (employees.emp_no = salaries.emp_no);
+ON employees.emp_no = salaries.emp_no;
 
 --2 List the first name, last name, and hire date for employees who were hired in 1986
 SELECT first_name, last_name, hire_date 
